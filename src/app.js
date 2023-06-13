d193328233a19458a6963f672ef7096b8cdbe5bd
Carlos Zambrano <emmanuelzzz123@gmail.com>
2023-06-13 10:06:19 -0700

    primer push

---
+++ src/app.js
@@ -0,0 +1,13 @@
+//app.js
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Pagina de Inicio de la App");
+});
+app.get("/login", (req, res) => {
+  res.send("Login");
+});
+
+module.export = app;
